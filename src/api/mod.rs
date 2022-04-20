a6eea7855647fd3fd875d5b4564ec28ec6776d94
Idontknow13 <pokeminecraftexpert@gmail.com>
2022-04-20 18:07:53 +0800

    :broom: Unused code cleanup

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,13 +2,8 @@ pub mod data;
 pub mod errors;
 use data::*;
 
-type Error = Box<dyn std::error::Error>;
-type Result<T> = std::result::Result<T, Error>;
-
-const URL: &'static str = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-
-pub fn define(word: &str) -> Result<Vec<Word>> {
-    let request_url = format!("{URL}{word}");
+pub fn define(word: &str) -> Result<Vec<Word>, Box<dyn std::error::Error>> {
+    let request_url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
     let response = reqwest::blocking::get(request_url)?.text()?;
     Ok(serde_json::from_str(response.as_str()).expect("Word Parsing Error"))
 }
