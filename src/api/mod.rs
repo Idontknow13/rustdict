494772edd465a8664ce5aae513c2018ebda9849b
Idontknow13 <pokeminecraftexpert@gmail.com>
2022-04-20 18:52:22 +0800

    :heavy_plus_sign: Added Display for Word

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,10 +1,33 @@
 pub mod data;
 use data::*;
 
+use std::fmt::{Display, Formatter};
+
 pub fn define(word: &str) -> Result<Vec<Word>, Box<dyn std::error::Error>> {
     let request_url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
     let response = reqwest::blocking::get(request_url)?.text()?;
 
     let data: Vec<Word> = serde_json::from_str(response.as_str())?;
     Ok(data)
+}
+
+impl Display for data::Word {
+    fn fmt(&self, fmtr: &mut Formatter) -> std::fmt::Result {
+        writeln!(fmtr, "")?;
+
+        for phonetic in self.phonetics.iter() {
+            if let Some(phoneme) = &phonetic.text {
+                writeln!(fmtr, "{}    {}", self.word, phoneme)?;
+            }
+        }
+
+        for meaning in self.meanings.iter() {
+            writeln!(fmtr, "- {}", meaning.part_of_speech)?;
+            for (num, definition) in meaning.definitions.iter().enumerate() {
+                writeln!(fmtr, "  {}: {}", num + 1, definition.definition)?;
+            }
+        }
+
+        Ok(())
+    }
 }
