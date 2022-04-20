01938bac3b664ddaab5f049984ade989085fe0fd
Idontknow13 <pokeminecraftexpert@gmail.com>
2022-04-20 18:17:42 +0800

    :broom: Improved Error Handling

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,5 +4,7 @@ use data::*;
 pub fn define(word: &str) -> Result<Vec<Word>, Box<dyn std::error::Error>> {
     let request_url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
     let response = reqwest::blocking::get(request_url)?.text()?;
-    Ok(serde_json::from_str(response.as_str()).expect("Word Parsing Error"))
+
+    let data: Vec<Word> = serde_json::from_str(response.as_str())?;
+    Ok(data)
 }
