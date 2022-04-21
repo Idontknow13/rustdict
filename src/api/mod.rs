62dc0d0eda7ce42b5dde81a0e8ae5fa6198fa444
Idontknow13 <pokeminecraftexpert@gmail.com>
2022-04-21 10:35:26 +0800

    :ballot_box_with_check: Added UrbanDict define

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -11,6 +11,14 @@ pub fn define(word: &str) -> Result<Vec<Word>, Box<dyn std::error::Error>> {
     Ok(data)
 }
 
+pub fn define_urban(word: &str) -> Result<UrbanContainer, Box<dyn std::error::Error>> {
+    let request_url = format!("https://api.urbandictionary.com/v0/define?term={word}");
+    let response = reqwest::blocking::get(request_url)?.text()?;
+
+    let data: UrbanContainer = serde_json::from_str(response.as_str())?;
+    Ok(data)
+}
+
 impl Display for Word {
     fn fmt(&self, fmtr: &mut Formatter) -> std::fmt::Result {
         writeln!(fmtr, "")?;
@@ -27,6 +35,26 @@ impl Display for Word {
             for (num, definition) in meaning.definitions.iter().enumerate() {
                 writeln!(fmtr, "  {}: {}", num + 1, definition.definition)?;
             }
+        }
+
+        Ok(())
+    }
+}
+
+impl Display for UrbanContainer {
+    fn fmt(&self, fmtr: &mut Formatter) -> std::fmt::Result {
+        writeln!(fmtr, "")?;
+
+        let main_word = &self.definitions[0].word;
+        writeln!(fmtr, "{main_word}")?;
+
+        for definition in self.definitions.iter() {
+            let clean_definition = definition
+                .definition
+                .chars()
+                .filter(|chr| !(chr == &'\r' || chr == &'\n' || chr == &'[' || chr == &']'))
+                .collect::<String>();
+            writeln!(fmtr, "  - {} (by: {})", clean_definition, definition.author)?;
         }
 
         Ok(())
